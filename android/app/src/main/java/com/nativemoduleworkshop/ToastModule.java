53a09f8c90376520db39c0c3d8883b6211cf397c
Kimssmin <chakane5@naver.com>
2023-08-18 23:27:12 +0900

    Chapter10 start

---
+++ android/app/src/main/java/com/nativemoduleworkshop/ToastModule.java
@@ -0,0 +1,27 @@
+package com.nativemoduleworkshop;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+    ToastModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @Override
+    public String getName() {
+        return "ToastModule";
+    }
+
+    @ReactMethod
+    public void show(String message, int duration) {
+        ReactApplicationContext context = getReactApplicationContext();
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+    }
+}
